1552d3b0e03281811c384de370d63a5658c6ab11
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-05-01 18:18:31 +0530

    Added Solution for Ass-3.3

---
+++ Assignment(3.3).py
@@ -0,0 +1,34 @@
+===========================================================================================================================================
+
+#Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error.
+#If the score is between 0.0 and 1.0, print a grade using the following table:
+#Score Grade
+#>= 0.9 A
+#>= 0.8 B
+#>= 0.7 C
+#>= 0.6 D
+#< 0.6 F
+#If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
+
+========================================================================================================================================
+
+========================================================================================================================================
+
+#Program begins here
+score = input("Enter Score: ")
+s= float(score)
+
+if s>=0.9 and s<1.0 :
+    print('A')
+elif s>=0.8 and s<0.9 :
+    print('B')
+elif s>=0.7 and s<0.8 :
+    print('C')
+elif s>=0.6 and s<0.7 :
+    print('D')
+elif s>=0.0 and s<0.6 :
+    print('F')
+else :
+    print('Error')
+    
+========================================================================================================================================
