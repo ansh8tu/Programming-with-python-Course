0ff16a50c8b08051665e9c2be8dccaf91508a3a9
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-06-21 20:46:20 +0530

    Added Program Description

--- Factorial.py
+++ Factorial.py
@@ -1,4 +1,4 @@
-# Python program to find the factorial of a number provided by the user.
+# This is a Python program to find the factorial of a number provided by the user.
 
 # change the value for a different result
 num = 7
