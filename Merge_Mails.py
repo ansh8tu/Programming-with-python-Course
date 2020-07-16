2d6bee8bdea3963bb4285bf81ca04de0cce26c03
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-07-17 00:33:09 +0530

    Added Program Description

--- Merge_Mails.py
+++ Merge_Mails.py
@@ -1,6 +1,4 @@
-# Python program to mail merger
-# Names are in the file names.txt
-# Body of the mail is in body.txt
+#This is a Python program to merge mails
 
 # open names.txt for reading
 with open("names.txt",'r',encoding = 'utf-8') as names_file:
