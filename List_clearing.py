c667f918e08e2f928a4f8e6398e46f04a1e37235
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-07-19 04:53:26 +0530

    Create List_clearing.py

---
+++ List_clearing.py
@@ -0,0 +1,27 @@
+# Python3 code to demonstrate clearing a list using clear and Reinitializing 
+
+# Initializing lists 
+list1 = [1, 2, 3] 
+list2 = [5, 6, 7] 
+
+# Printing list1 before deleting 
+print ("List1 before deleting is : "
++ str(list1)) 
+
+# deleting list using clear() 
+list1.clear() 
+
+# Printing list1 after clearing 
+print ("List1 after clearing using clear() : "
++ str(list1)) 
+
+# Printing list2 before deleting 
+print ("List2 before deleting is : "
++ str(list2)) 
+
+# deleting list using reinitialization 
+list2 = [] 
+
+# Printing list2 after reinitialization 
+print ("List2 after clearing using reinitialization : "
++ str(list2)) 
