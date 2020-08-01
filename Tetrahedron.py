b46575ddc38912e2742d8f786d1ff7a341765710
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-08-01 23:28:57 +0530

    Create Tetrahedron.py

---
+++ Tetrahedron.py
@@ -0,0 +1,10 @@
+# Python code to find the volume of a tetrahedron 
+import math 
+def vol_tetra(side): 
+    volume = (side ** 3 / (6 * math.sqrt(2))) 
+    return round(volume, 2) 
+  
+# Driver Code 
+side = 3
+vol = vol_tetra(side) 
+print(vol)
