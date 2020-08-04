d7bd7ed365fd29157166e7668e7df5e05715c749
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-08-05 05:00:27 +0530

    Added Program Description

--- Perimeter_of_cylinder.py
+++ Perimeter_of_cylinder.py
@@ -1,6 +1,5 @@
 
-# Function to calculate  
-# the perimeter of a cylinder 
+#This is a python program to calculate the perimeter of a cylinder
 def perimeter( diameter, height ) : 
     return 2 * ( diameter + height )  
   
