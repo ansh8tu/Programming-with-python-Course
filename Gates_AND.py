c1017e9fdc6d64650672ddbbd6e13813876fc829
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-09-04 19:45:31 +0530

    Create Gates_AND.py

---
+++ Gates_AND.py
@@ -0,0 +1,19 @@
+# Python3 program to illustrate working of AND gate
+
+def AND (a, b):
+
+	if a == 1 and b == 1:
+		return True
+	else:
+		return False
+
+# Driver code
+if __name__=='__main__':
+	print(AND(1, 1))
+
+	print("+---------------+----------------+")
+	print(" | AND Truth Table | Result |")
+	print(" A = False, B = False | A AND B =",AND(False,False)," | ")
+	print(" A = False, B = True | A AND B =",AND(False,True)," | ")
+	print(" A = True, B = False | A AND B =",AND(True,False)," | ")
+	print(" A = True, B = True | A AND B =",AND(True,True)," | ")
